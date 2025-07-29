89479794618e823ecb671e374ac22c50696f41d0
arghavan <arghavan.vg@gmail.com>
2025-07-29 14:25:26 -0400

    Add: meniscus for y axis

---
+++ meniscus-y-axis.py
@@ -0,0 +1,114 @@
+import mdtraj as md
+import numpy as np
+# from scipy.spatial.distance import pdist, squareform
+# import parmed as pmd
+from warnings import filterwarnings
+filterwarnings('ignore')
+# import logging
+import os
+import sys
+
+t = int(sys.argv[1])
+d = sys.argv[2]
+
+input_path = f'/Users/arghavan/Graduate Center Dropbox/Arghavan Vedadi Gargari/MyFiles/{t}K/{d}/'
+output_path = f'/Users/arghavan/lab/hp-results/meniscus-results/{t}K/{d}/'
+mean_nbr_path = f'{input_path}all_frame_mean_neighbours.pkl'
+Findx_path = f'{input_path}Findx_N_indices_neighbours_hbonds.pkl'
+
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+# input_path = f'/gibbs/arghavan/counting_hbonds_between_graphite_walls/hbond_inputs/{t}K/{d}/'
+# output_path = f'/gibbs/arghavan/pair_energy/results_pair_energy/{t}K_{d}/'
+# mean_nbr_path = f'/gibbs/arghavan/counting_hbonds_between_graphite_walls/hbond_outputs/{t}K/{d}/all_frame_mean_neighbours.pkl'
+
+traj_path = f'{input_path}{t}K_{d}.nc'
+top_path = f'{input_path}topol_edited.prmtop'
+
+traj = md.load(traj_path, top=top_path)
+tpl = traj.top
+all_ox_tpl_indices = tpl.select("name O")
+all_wat_atm_tpl_indices = tpl.select("water")
+walls = tpl.select("resname =~ 'WALL'")
+
+n_frames = traj.n_frames
+# n_frames = 50
+
+wall_coords = traj.xyz[0][walls]
+
+float_d = float(d)
+x_center = 2.50
+x_min, x_max = np.float32(x_center - float_d/2), np.float32(x_center + float_d/2)
+y_min, y_max = np.min(wall_coords[:,1]), np.max(wall_coords[:,1])
+z_min, z_max = np.min(wall_coords[:,2]), np.max(wall_coords[:,2])
+
+
+
+slice_thickness = 0.010
+
+n_slices = int((y_max - y_min) / slice_thickness)
+y_starts = np.array([round(y_min + i * slice_thickness, 3) for i in range(n_slices)])
+y_ends = y_starts + slice_thickness
+y_distances = [round((i + 1) * slice_thickness * 10, 1) for i in range(len(y_starts))]
+
+y_counts = [[] for _ in range(len(y_starts))]
+
+# Loop through frames
+for frame_num in range(n_frames):
+    coords = traj.xyz[frame_num][all_ox_tpl_indices]
+
+    for i, (y_start, y_end) in enumerate(zip(y_starts, y_ends)):
+        mask = (
+            (coords[:, 0] >= x_min) & (coords[:, 0] < x_max) &
+            (coords[:, 1] >= y_start) & (coords[:, 1] < y_end) &
+            (coords[:, 2] >= z_min) & (coords[:, 2] < z_max)
+        )
+        y_counts[i].append(np.sum(mask))
+
+y_data = [[float(y_dist), float(np.mean(counts))] for y_dist, counts in zip(y_distances, y_counts)]
+np.savetxt(f"{output_path}counts-y.dat", y_data, fmt="%.1f\t%.8f")
+
+v = float_d * 10 * slice_thickness * 10 * 17.62 #A^3
+numdens_data = [[float(y_dist), float(np.mean(counts))/v] for y_dist, counts in zip(y_distances, y_counts)]
+np.savetxt(f"{output_path}numdens-y.dat", numdens_data, fmt="%.1f\t%.5f")
+
+
+x_axis = [float(y_dist) for y_dist, _ in y_data]
+no_wats = [float(count) for _, count in y_data]
+numdens = [float(dens) for _, dens in numdens_data]
+
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+
+plt.figure(figsize=(8, 6))
+plt.plot(x_axis, no_wats, color='blue', linewidth=2, label=f"Plates distance = {float_d*10:.1f} Å")
+# plt.plot(bin_centers, rho_i_plate, color='green', linewidth=2, label=f"{float(d)*10:.1f} Å")
+plt.title("Water Distribution between the edges")
+plt.xlabel('Distance from the edge in Å', fontsize=14)
+plt.ylabel('Average Count', fontsize=14)
+plt.legend()
+plt.xlim(0, 18)
+plt.ylim(bottom=0)
+# plt.xticks([-4.0, -2.0, 0.0, 2.0])
+plt.gca().xaxis.set_minor_locator(MultipleLocator(0.5))
+plt.tick_params(axis='x', which='minor', length=2)
+plt.tight_layout()
+plt.savefig(f"{output_path}/counts-y.png", dpi=300)
+plt.close()
+
+
+plt.figure(figsize=(8, 6))
+plt.plot(x_axis, numdens, color='blue', linewidth=2, label=f"Plates distance = {float_d*10:.1f} Å")
+plt.title("Number Density Distribution between the edges")
+plt.xlabel('Distance from the edge in Å', fontsize=14)
+plt.ylabel('Number Density N/(Å^3)', fontsize=14)
+plt.legend()
+plt.xlim(0, 18)
+plt.ylim(bottom=0)
+# plt.xticks([-4.0, -2.0, 0.0, 2.0])
+plt.gca().xaxis.set_minor_locator(MultipleLocator(0.5))
+plt.tick_params(axis='x', which='minor', length=2)
+plt.tight_layout()
+plt.savefig(f"{output_path}/numDens-y.png", dpi=300)
+plt.close()
